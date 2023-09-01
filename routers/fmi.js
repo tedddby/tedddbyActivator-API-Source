03df85e58f1a608eb3ed60c8d8d2757e698acc0c
tedddby <katotzu@gmail.com>
2023-09-01 16:24:33 -0700

    Update fmi.js

--- routers/fmi.js
+++ routers/fmi.js
@@ -3,7 +3,7 @@ const router = express.Router();
 const multer = require("multer");
 const upload = multer();
 
-const controller = require("../controllers/fmi.js");
+const controller = require("../controllers/fmi/fmi.js");
 
 router.post("/register", controller.register);
 
